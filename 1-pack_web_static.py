2192138bcc6fa6ef29c70339af6d90de31616678
BennettDixon <bennettdixon16@gmail.com>
2019-04-08 17:36:38 +0000

    forced tar to use gzip

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,14 @@
 from fabric.api import *
 from datetime import datetime
 
+
 def do_pack():
     """packages all contents from web_static into .tgz archive
     """
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    result = path = local('tar -cvf versions/web_static_{}.tgz web_static'.format(now))
+    result = local('tar -czvf versions/web_static_{}.tgz web_static'
+                   .format(now))
     if result.failed:
         return None
     else:
